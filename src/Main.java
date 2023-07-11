d8a90477bf9ccd23a72392c612b1547250300e88
Vladimir Shpachenko <kniyzvladimir@gmail.com>
2023-07-11 21:50:13 +0300

    2-nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.ListIterator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String s = "I believe that the planet from which the little prince came is Asteroid B-612";
+        String [] array = s.split(" ");
+        List<String> list = Arrays.asList(array);
+        ListIterator<String> listIterator = list.listIterator();
+        while (listIterator.hasNext()) {
+            System.out.println(listIterator.next());
+        }
     }
 }
